f80d8a3db598ec7d8b0cde05e57b7f93a97862fe
KT924 <736732188@qq.com>
2020-09-01 15:10:00 +0800

    修改了上传文件的页面逻辑

--- BatchOperation/views.py
+++ BatchOperation/views.py
@@ -8,17 +8,22 @@ from .CallScript import call_script
 def batch_operation(request):
     if request.method=='POST':
         businessType=request.POST['business-type']
-        batchFile=request.FILES.get('batch_file')
+        batchFile=request.FILES.getlist('batch_file')
         # print(type(batchFile))
         # print(businessType)
         # filepath=os.path.join(r"/ocs/opsadmin/ScriptOps/BatchOperation/batch_fille",batchFile.name)
         filepath="/ocs/opsadmin/ScriptOps/BatchOperation/batch_fille"
+        result=[]
         for f in batchFile:
             dest=open(os.path.join(filepath,f.name),mode='wb')
-            for c in batchFile.chunks():
-                dest.write()
+            for c in f.chunks():
+                dest.write(c)
             dest.close()
-        return HttpResponse("<h2>文件上传成功</h2>")
+            result.append((f.name,os.path.exists(os.path.join(filepath,f.name))))
+        
+        context={'result':result}
+        # return HttpResponse("<h2>文件上传成功</h2>")
+        return render(request,'batch_operation.html',context)
     return render(request,'batch_operation.html')
 
 def sql_operation(request):
